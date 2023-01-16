df4362764596da85498b02505b64c634e089ef75
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-16 10:48:40 -0800

    Add toString ability to display linkedlist in string format and ability to preappend nodes

--- main.js
+++ main.js
@@ -12,11 +12,46 @@ class LinkedList {
         this.size = 0
     }
 
-    append(value){
+
+
+    prepend(value){
         this.head = new Node(value, this.head)
+        this.size++
+    }
+
+    append(value){
+        let node = new Node(value)
+        let current
+        if(!this.head){
+            this.head = node
+        }else{
+            current = this.head
+
+            while(current.next){
+                current = current.next
+            }
+            current.next = node
+        }
+
+        this.size++
+    }
+
+    toString(){
+        let current = this.head
+        while(current){
+            console.log(current.value)
+            current = current.next
+        }
     }
 
 }
 
 const linko = new LinkedList()
+
+
+linko.prepend(10)
+linko.append(200)
+
 console.log(linko)
+
+linko.toString()
